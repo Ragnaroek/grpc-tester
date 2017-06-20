b58248e69c1f8543a8c67acc658e5a6137b78099
Michael Bohn <michael.bohn@freiheit.com>
2017-06-20 07:10:34 +0200

    established a valid to connection to test grpc server

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,19 @@ fn main() {
         Header::new(Bytes::from_static(b":method"), Bytes::from_static(b"POST")),
         Header::new(Bytes::from_static(b":path"), Bytes::from_static(b"/helloworld.Greeter/SayHello")),
         Header::new(Bytes::from_static(b":authority"), Bytes::from_static(b"localhost")),
-        Header::new(Bytes::from_static(b":scheme"), Bytes::from_static(b"https")),
+        Header::new(Bytes::from_static(b":scheme"), Bytes::from_static(b"http")),
         Header::new(Bytes::from_static(b"content-type"), Bytes::from_static(b"application/grpc")),
     ]);
 
-    let client = httpbis::Client::new_tls::<tls_api_openssl::TlsConnector>("localhost", 50051, Default::default()).expect("Client");
+    println!("header {:?}", headers);
+
+    let client = httpbis::Client::new_plain("localhost", 50051, Default::default()).expect("Client");
+
+    let mut mem = Bytes::from(&b"Hello world"[..]);
+    let response = client.start_request_simple(headers, mem);
+
+    println!("Response {:?}", response.collect().wait().unwrap().dump());
 
     //TODO start_request_simple with correct GRPC request bytes!
-    let resp = client.start_get("/helloworld.Greeter/SayHello", "localhost").collect().wait().expect("execute request");
+    //let resp = client.start_get("/helloworld.Greeter/SayHello", "localhost").collect().wait().expect("execute request");
 }
