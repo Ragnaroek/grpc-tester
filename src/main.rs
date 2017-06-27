75f13e558acbf3f606fa50117107723cb22d08ad
Michael Bohn <michael.bohn@freiheit.com>
2017-06-27 06:29:37 +0200

    define command line parameters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+extern crate clap;
+
 mod grpc;
 
+use clap::{Arg, App};
+
 fn main() {
-    grpc::send_dummy_request();
+
+    let matches = App::new("grpc-tester")
+                    .version("0.0.1")
+                    .about("Easy GRPC testing")
+                    .arg(Arg::with_name("proto")
+                          .required(true)
+                          .short("p")
+                          .long("proto")
+                          .value_name("PROTO_FILE")
+                          .help("proto file with service and message definition")
+                          .takes_value(true))
+                    .arg(Arg::with_name("serveraddr")
+                          .required(true)
+                          .short("s")
+                          .long("serveraddr")
+                          .value_name("SERVER_ADDR")
+                          .help("http address to GRPC server")
+                          .takes_value(true))
+                    .arg(Arg::with_name("rpcmessage")
+                          .required(true)
+                          .short("m")
+                          .long("rpcmessage")
+                          .help("name of the rpc message")
+                          .takes_value(true))
+                    .get_matches();
+
+    println!("matches {:?}", matches);
+    //grpc::send_dummy_request();
 }
